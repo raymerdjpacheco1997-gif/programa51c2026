f7de3e5c903fd466406d813a0a63c01eac1f2c8d
Ronald Ponce <geosist5@gmail.com>
2026-01-31 21:04:11 -0500

    Se valido a entero, agrego suma y muestra resultado

--- operacion_2.py
+++ operacion_2.py
@@ -1,7 +1,10 @@
 # Programa que pide dos valores y los imprime
 
-valor1 = input("Ingrese el primer valor: ")
-valor2 = input("Ingrese el segundo valor: ")
+valor1 = int(input("Ingrese el primer valor: "))
+valor2 = int(input("Ingrese el segundo valor: "))
 
 print("El primer valor ingresado es:", valor1)
 print("El segundo valor ingresado es:", valor2)
+
+suma=valor1 +valor2
+print ("la suma de los dos numeros es: ",suma)

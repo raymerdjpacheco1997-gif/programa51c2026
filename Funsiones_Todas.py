7b630f166ceab31117a544b2c9999fbc83c434d2
Eduardo Jose <ej861066@gmail.com>
2026-02-09 10:04:30 -0500

    Agregadas funciones de suma y promedio del Par 2 por Edwin Espinosa, Eduardo Salinas - G2

--- Funsiones_Todas.py
+++ Funsiones_Todas.py
@@ -1,6 +1,31 @@
-# Evaluación Parcial 01 - Profesor Ronald Ponce
-# Programa establecido para entrega de funsiones de los 7 Grupos
-print("Sean toos bienvenodos estimadoa estudiantes")
-print ("Muy buena suerte y exitos en su parcial_01")
+def suma_lista(lista):
+    """
+    Recibe una lista de números y calcula la suma total
+    """
+    total = 0
+    for numero in lista:
+        total = total + numero
+    return total
 
-# De aqui en adelante integre sus trabajos del parcial
+# Ejemplo de uso
+numeros = [10, 20, 30, 40, 50]
+resultado = suma_lista(numeros)
+print("Eduardo Salinas - Suma total:", resultado)
+
+#Edwin Espinosa 8-1023-293
+def calcular_promedio(lista):
+    if not lista:  # Verificamos que la lista no esté vacía para evitar error de división por cero
+        return 0
+    
+    suma_total = sum(lista)
+    cantidad_elementos = len(lista)
+    promedio = suma_total / cantidad_elementos
+    
+    return promedio
+
+# Ejemplo de uso:
+mis_numeros = [10, 8, 9, 7, 10]
+resultado = calcular_promedio(mis_numeros)
+
+print(f"La lista es: {mis_numeros}")
+print(f"El promedio es: {resultado}")
